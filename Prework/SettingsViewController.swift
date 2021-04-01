81456a3a08f83b555ad497ab08acafc5a649c454
isaacjperez <73368486+isaacjperez@users.noreply.github.com>
2021-03-31 20:57:39 -0500

    initial commit

---
+++ Prework/SettingsViewController.swift
@@ -0,0 +1,72 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Isaac Perez on 3/30/21.
+//
+
+import UIKit
+
+//keys for defaults.set
+//placed here to make accessible to viewController
+let tip1 = "tip1"
+let tip2 = "tip2"
+let tip3 = "tip3"
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tip1Field: UITextField!
+    @IBOutlet weak var tip2Field: UITextField!
+    @IBOutlet weak var tip3Field: UITextField!
+    
+    
+    
+    //get the values from the custom tip fields as integers
+    lazy var tip1Value = Int(tip1Field.text!) ?? 0
+    lazy var tip2Value = Int(tip2Field.text!) ?? 0
+    lazy var tip3Value = Int(tip3Field.text!) ?? 0
+    
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //make sure custom tip fields hold thier value when
+        //switching back to the settings page
+        let updatedTip1 : Int = Int(defaults.float(forKey: tip1))
+        let updatedTip2 : Int = Int(defaults.float(forKey: tip2))
+        let updatedTip3 : Int = Int(defaults.float(forKey: tip3))
+        
+        tip1Field.text = "\(updatedTip1)"
+        tip2Field.text = "\(updatedTip2)"
+        tip3Field.text = "\(updatedTip3)"
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        //as we transition back to the main screen
+        //save what is in the tip textfields
+        defaults.set(tip1Value, forKey: tip1)
+        defaults.set(tip2Value, forKey: tip2)
+        defaults.set(tip3Value, forKey: tip3)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+}
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var splitSlider: UISlider!
+    @IBOutlet weak var tipPerPersonLabel: UILabel!
+    
+    let defaults = UserDefaults.standard
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // user doesn't have to click in billAmountTextField
+        // key board is automatically brought up so they can enter amount
+        self.billAmountTextField.becomeFirstResponder()
+        
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //call calculateTip to update labels when switching back to calculator
+        calculateTip(self)
+        //make sure the tip control lables mathc the custom labels
+        tipControl.setTitle("\(defaults.integer(forKey: tip1))%", forSegmentAt:0)
+        tipControl.setTitle("\(defaults.integer(forKey: tip2))%", forSegmentAt:1)
+        tipControl.setTitle("\(defaults.integer(forKey: tip3))%", forSegmentAt:2)
+    }
+    
+   
+    @IBAction func calculateTip(_ sender: Any) {
+        //get the bill amount
+        let bill = Double(billAmountTextField.text!) ?? 0
+       
+        //calculate the tip and total
+        //divid by 100 to get the percentage
+        let tipPercentages = [ Double(defaults.float(forKey: tip1))/100.0,
+                               Double (defaults.float(forKey: tip2))/100.0,
+                               Double (defaults.float(forKey: tip3))/100.0]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update tip and total labels
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        // round the value from the slider
+        // so that tipPerPerson doesnt change before it get to the next whole number on the slider
+        let personCount = round(splitSlider.value)
+        let tipPerPerson: Double = tip / Double(personCount)
+        
+        tipPerPersonLabel.text = String(format: "$%.2f Per Person", tipPerPerson)
+        
+    
+    }
+    
+    @IBAction func tipSplitter(_ sender: Any) {
+        
+        // source: https://www.journaldev.com/22833/ios-uislider begin
+        // make the slider stop at intervals of 1
+        let uiSlider = sender as! UISlider
+        let step:Float = 1
+        let roundedStepValue = round(uiSlider.value / step) * step
+        uiSlider.value = roundedStepValue
+        // source end
+        if(Int(splitSlider.value) == 1){
+            splitLabel.text = "Split 1 Way"
+        }else{
+            splitLabel.text = String(format: "Split %i Ways", Int(splitSlider.value))
+        }
+        
+    }
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
 }
 
